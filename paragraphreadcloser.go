8aab9ea482dbbd024a8d59ea28e3387724f018c9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-30 14:32:47 -0700

    paragraphreadcloser

---
+++ paragraphreadcloser.go
@@ -0,0 +1,255 @@
+package iopsep
+
+import (
+	"errors"
+	"io"
+
+	"github.com/reiver/go-unicode"
+	"sourcecode.social/reiver/go-utf8"
+)
+
+type internalParagraphReadCloser struct {
+	reader io.Reader
+	pipereader *io.PipeReader
+	pipewriter *io.PipeWriter
+}
+
+func NewParagraphReadCloser(reader io.Reader) io.ReadCloser {
+
+	if nil == reader {
+		return nil
+	}
+
+	pipereader, pipewriter := io.Pipe()
+	if nil == pipereader {
+		return nil
+	}
+	if nil == pipewriter {
+		return nil
+	}
+
+	paragraphreader := internalParagraphReadCloser{
+		reader:reader,
+		pipereader:pipereader,
+		pipewriter:pipewriter,
+	}
+
+	go paragraphreader.pipewrite()
+
+	return &paragraphreader
+}
+
+func (receiver *internalParagraphReadCloser) Close() error {
+	if nil == receiver {
+		return errNilReceiver
+	}
+
+	{
+		var pipewriter *io.PipeWriter = receiver.pipewriter
+		if nil != pipewriter {
+			
+		}
+	}
+
+	{
+		var pipereader *io.PipeReader = receiver.pipereader
+		if nil != pipereader {
+			err := pipereader.Close()
+			if nil != err {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (receiver *internalParagraphReadCloser) Read(p []byte) (n int, err error) {
+	if nil == receiver {
+		return 0, errNilReceiver
+	}
+
+	var reader io.Reader = receiver.reader
+	if nil == reader {
+		return 0, errNilReader
+	}
+
+	var pipereader io.Reader = receiver.pipereader
+	if nil == pipereader {
+		return 0, errNilPipeReader
+	}
+
+	return pipereader.Read(p)
+}
+
+func (receiver *internalParagraphReadCloser) pipewrite() {
+	if nil == receiver {
+		panic(errNilReceiver)
+	}
+
+	var pipewriter *io.PipeWriter = receiver.pipewriter
+	if nil == pipewriter {
+		panic(errNilPipeWriter)
+	}
+
+	var reader io.Reader = receiver.reader
+	if nil == reader {
+		err := pipewriter.CloseWithError(errNilReader)
+		if nil != err {
+			panic(err)
+		}
+	}
+
+	pipewrite(receiver.writerune, receiver.returneof, receiver.returnerror, reader)
+}
+
+func (receiver *internalParagraphReadCloser) returneof() {
+	if nil == receiver {
+		panic(errNilWriter)
+	}
+
+	var pipewriter *io.PipeWriter = receiver.pipewriter
+	if nil == pipewriter {
+		panic(errNilPipeWriter)
+	}
+
+	{
+		err := pipewriter.CloseWithError(io.EOF)
+		if nil != err {
+			panic(err)
+		}
+	}
+}
+
+func (receiver *internalParagraphReadCloser) returnerror(err error) {
+	if nil == receiver {
+		panic(errNilWriter)
+	}
+
+	var pipewriter *io.PipeWriter = receiver.pipewriter
+	if nil == pipewriter {
+		panic(errNilPipeWriter)
+	}
+
+	{
+		err := pipewriter.CloseWithError(err)
+		if nil != err {
+			panic(err)
+		}
+	}
+}
+
+func (receiver *internalParagraphReadCloser) writerune(r rune) (exit bool) {
+	if nil == receiver {
+		panic(errNilWriter)
+	}
+
+	var pipewriter *io.PipeWriter = receiver.pipewriter
+	if nil == pipewriter {
+		panic(errNilPipeWriter)
+	}
+
+	{
+		_, err := utf8.WriteRune(pipewriter, r)
+		if errors.Is(err, io.ErrClosedPipe) {
+			pipewriter.Close()
+			return true
+		}
+		if nil != err {
+			e := pipewriter.CloseWithError(err)
+			if nil != e {
+				panic(e)
+			}
+		}
+	}
+
+	return false
+}
+
+func pipewrite(writerune func(rune)bool, returneof func(), returnerror func(error), reader io.Reader) {
+
+	if nil == writerune {
+		panic(errNilWriteRuneFunction)
+	}
+
+	if nil == returneof {
+		panic(errNilReturnEOFFunction)
+	}
+
+	if nil == returnerror {
+		panic(errNilReturnErrorFunction)
+	}
+
+	var prevcr bool
+	var preveol bool
+	var prevlf bool
+
+	for {
+		r, size, err := utf8.ReadRune(reader)
+		if 0 < size {
+			exit := writerune(r)
+			if exit {
+				return
+			}
+
+			switch r {
+			case unicode.LF:
+				switch {
+				case prevlf:
+					returneof()
+					return
+				case prevcr && preveol:
+					returneof()
+					return
+				case prevcr:
+					prevcr  = false
+					preveol = true
+					prevlf  = false
+				default:
+					prevcr  = false
+					prevlf  = true
+				}
+			case unicode.CR:
+				switch {
+				case prevcr:
+					returneof()
+					return
+				case prevlf && preveol:
+					returneof()
+					return
+				case prevlf:
+					prevcr  = false
+					preveol = true
+					prevlf  = false
+				default:
+					prevcr  = true
+					prevlf  = false
+				}
+			case unicode.NEL:
+				if preveol {
+					returneof()
+					return
+				}
+				prevcr  = false
+				preveol = true
+				prevlf  = false
+			case unicode.PS:
+				returneof()
+				return
+			default:
+				prevcr  = false
+				preveol = false
+				prevlf  = false
+			}
+		}
+		if io.EOF == err {
+			returneof()
+			return
+		}
+		if nil != err {
+			returnerror(err)
+			return
+		}
+	}
+
+}
---
+++ pipewrite_test.go
@@ -0,0 +1,189 @@
+package iopsep
+
+import (
+	"testing"
+
+	"io"
+	"strings"
+)
+
+func TestPipeWrite(t *testing.T) {
+
+	tests := []struct{
+		Data string
+		Expected string
+	}{
+		{
+			Data: "",
+			Expected: "",
+		},
+
+
+
+		{
+			Data:     "Hello world!\n\nOnce Twice Thrice Fource",
+			Expected: "Hello world!\n\n",
+		},
+		{
+			Data:     "Hello world!\n\r\n\rOnce Twice Thrice Fource",
+			Expected: "Hello world!\n\r\n\r",
+		},
+		{
+			Data:     "Hello world!\n\r\u0085Once Twice Thrice Fource",
+			Expected: "Hello world!\n\r\u0085",
+		},
+		{
+			Data:     "Hello world!\r\rOnce Twice Thrice Fource",
+			Expected: "Hello world!\r\r",
+		},
+		{
+			Data:     "Hello world!\r\n\r\nOnce Twice Thrice Fource",
+			Expected: "Hello world!\r\n\r\n",
+		},
+		{
+			Data:     "Hello world!\r\n\u0085Once Twice Thrice Fource",
+			Expected: "Hello world!\r\n\u0085",
+		},
+		{
+			Data:     "Hello world!\u0085\u0085Once Twice Thrice Fource",
+			Expected: "Hello world!\u0085\u0085",
+		},
+		{
+			Data:     "Hello world!\u0085\n\rOnce Twice Thrice Fource",
+			Expected: "Hello world!\u0085\n\r",
+		},
+		{
+			Data:     "Hello world!\u0085\r\nOnce Twice Thrice Fource",
+			Expected: "Hello world!\u0085\r\n",
+		},
+		{
+			Data:     "Hello world!\u2029Once Twice Thrice Fource",
+			Expected: "Hello world!\u2029",
+		},
+
+
+
+		{
+			Data:     "Hello world!\n\n",
+			Expected: "Hello world!\n\n",
+		},
+		{
+			Data:     "Hello world!\n\r\n\r",
+			Expected: "Hello world!\n\r\n\r",
+		},
+		{
+			Data:     "Hello world!\r\r",
+			Expected: "Hello world!\r\r",
+		},
+		{
+			Data:     "Hello world!\r\n\r\n",
+			Expected: "Hello world!\r\n\r\n",
+		},
+		{
+			Data:     "Hello world!\u0085\u0085",
+			Expected: "Hello world!\u0085\u0085",
+		},
+		{
+			Data:     "Hello world!\u2029",
+			Expected: "Hello world!\u2029",
+		},
+
+
+
+		{
+			Data:     "\n\n",
+			Expected: "\n\n",
+		},
+		{
+			Data:     "\n\r\n\r",
+			Expected: "\n\r\n\r",
+		},
+		{
+			Data:     "\r\r",
+			Expected: "\r\r",
+		},
+		{
+			Data:     "\r\n\r\n",
+			Expected: "\r\n\r\n",
+		},
+		{
+			Data:     "\u0085\u0085",
+			Expected: "\u0085\u0085",
+		},
+		{
+			Data:     "\u2029",
+			Expected: "\u2029",
+		},
+
+
+
+		{
+			Data:     "\n\nOnce Twice Thrice Fource",
+			Expected: "\n\n",
+		},
+		{
+			Data:     "\n\r\n\rOnce Twice Thrice Fource",
+			Expected: "\n\r\n\r",
+		},
+		{
+			Data:     "\r\rOnce Twice Thrice Fource",
+			Expected: "\r\r",
+		},
+		{
+			Data:     "\r\n\r\nOnce Twice Thrice Fource",
+			Expected: "\r\n\r\n",
+		},
+		{
+			Data:     "\u0085\u0085Once Twice Thrice Fource",
+			Expected: "\u0085\u0085",
+		},
+		{
+			Data:     "\u2029Once Twice Thrice Fource",
+			Expected: "\u2029",
+		},
+	}
+
+	for testNumber, test := range tests {
+		var buffer strings.Builder
+		writerune := func(r rune)(exit bool) {
+			buffer.WriteRune(r)
+			return false
+		}
+
+		var eof bool
+		returneof := func() {
+			eof = true
+		}
+
+		returnerror := func(err error) {
+			t.Errorf("For test #%d, did not expect a 'return error' but actually got one." , testNumber)
+			t.Logf("ERROR: (%T) %s", err, err)
+			t.Logf("DATA: %q", test.Data)
+			t.Logf("BUFFER: %q", buffer.String())
+		}
+
+		var reader io.Reader = strings.NewReader(test.Data)
+
+		pipewrite(writerune, returneof, returnerror, reader)
+
+		{
+			if !eof {
+				t.Errorf("For test #%d, expected 'eof' but did not actually get one." , testNumber)
+				t.Logf("DATA: %q", test.Data)
+				t.Logf("BUFFER: %q", buffer.String())
+			}
+		}
+
+		{
+			expected := test.Expected
+			actual   := buffer.String()
+
+			if expected != actual {
+				t.Errorf("For test #%d, the actual 'written-runes' is not what was expected" , testNumber)
+				t.Logf("EXPECTED: %q", expected)
+				t.Logf("ACTUAL:   %q", actual)
+				t.Logf("DATA:     %q", test.Data)
+			}
+		}
+	}
+}
